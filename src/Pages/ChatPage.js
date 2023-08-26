00c8a043549c2155e7193f81574ebea6513d356c
Prajay1610 <prajaypc16@gmail.com>
2023-08-26 10:54:19 +0530

    .

--- src/Pages/ChatPage.js
+++ src/Pages/ChatPage.js
@@ -1,28 +1,31 @@
-import {ChatState} from "../Context/ChatProvider";
-import {Box} from '@chakra-ui/react';
-import SideDrawer from '../components/miscellaneous/SideDrawer';
-import ChatBox from '../components/ChatBox';
-import MyChats from '../components/MyChats';
-
+import { ChatState } from "../Context/ChatProvider";
+import { Box } from "@chakra-ui/react";
+import SideDrawer from "../components/miscellaneous/SideDrawer";
+import ChatBox from "../components/ChatBox";
+import MyChats from "../components/MyChats";
+import React, { useEffect, useState } from "react";
 
 const ChatPage = () => {
-   const {user} = ChatState();
-   
-    return(
-      <div style={{width:"100%"}}>
-      {user && <SideDrawer/>}
-      <Box 
-      display='flex'
-      justifyContent="space-between" 
-      w="100%" 
-      h="91.5vh" 
-      p="10px"
-     >
-        {user && <MyChats/>}
-        {user && <ChatBox/>}
+  const { user } = ChatState();
+  const [fetchAgain, setFetchAgain] = useState(false);
+
+  return (
+    <div style={{ width: "100%" }}>
+      {user && <SideDrawer />}
+      <Box
+        display="flex"
+        justifyContent="space-between"
+        w="100%"
+        h="91.5vh"
+        p="10px"
+      >
+        {user && <MyChats fetchAgain={fetchAgain} />}
+        {user && (
+          <ChatBox fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+        )}
       </Box>
     </div>
-    );
+  );
 };
 
 export default ChatPage;
---
+++ src/components/SingleChat.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { ChatState } from "../Context/ChatProvider";
+import { Box, IconButton, Text } from "@chakra-ui/react";
+import { ArrowBackIcon } from "@chakra-ui/icons";
+
+const SingleChat = ({ fetchAgain, setFetchAgain }) => {
+  const { user, selectedChat, setSelectedChat } = ChatState();
+  return (
+    <>
+      {selectedChat ? (
+        <>
+          <Text
+            fontSize={{ base: "28px", md: "30px" }}
+            pb={3}
+            px={2}
+            w="100%"
+            fontFamily="Work sans"
+            display="flex"
+            justifyContent={{ base: "space-between" }}
+            alignItems="center"
+          >
+            <IconButton
+              display={{ base: "flex", md: "none" }}
+              icon={<ArrowBackIcon />}
+              onClick={() => setSelectedChat("")}
+            />
+          </Text>
+        </>
+      ) : (
+        <Box
+          display="flex"
+          alignItems="center"
+          justifyContent="center"
+          h="100%"
+        >
+          <Text fontSize="3xl" pb={3} fontfamily="Work sans">
+            Click on a user to start chatting
+          </Text>
+        </Box>
+      )}
+    </>
+  );
+};
+
+export default SingleChat;
